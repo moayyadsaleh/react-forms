84a1fb1bb84a696f325ee6f2469c5da30f824830
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-09-03 11:19:36 -0600

    Adding form to the front end

---
+++ src/components/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+import "../styles.css";
+
+function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    setHeading(name);
+    event.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText} </h1>
+      <form onSubmit={handleClick}>
+        {" "}
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit" onClick={handleClick}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
